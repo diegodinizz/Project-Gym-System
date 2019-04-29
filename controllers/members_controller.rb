377ef00e955c1960237a2901e21369cc7221fab3
Diego Diniz da Silva <diegodinizz@gmail.com>
2019-04-29 10:47:04 +0100

    working on members edit

--- controllers/members_controller.rb
+++ controllers/members_controller.rb
@@ -9,8 +9,32 @@ get '/members' do
   erb(:"members/index")
 end
 
+# new
+get '/members/new' do
+  erb(:"members/new")
+end
+
 # show
 get '/members/:id' do
   @member = Member.find(params[:id])
   erb(:"members/show")
+end
+
+# create
+post '/members/sigh-up' do
+  @members = Member.new(params)
+  @members.save()
+  erb(:"members/create")
+end
+
+# edit
+get '/members/:id/edit' do
+  @members = Member.find(params[:id])
+  erb(:"/members/edit")
+end
+
+# update
+post '/members/:id' do
+  Member.new(params).update
+  redirect to '/members'
 end
