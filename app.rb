7bf7732f8cb57063af99661cbb33d6bf655f8c75
Diego Diniz da Silva <diegodinizz@gmail.com>
2019-08-26 14:03:24 +0100

    comment out contrib/all

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require( 'sinatra' )
-require( 'sinatra/contrib/all' )
+# require( 'sinatra/contrib/all' )
 require_relative('controllers/attendences_controller.rb')
 require_relative('controllers/gymclasses_controller.rb')
 require_relative('controllers/members_controller.rb')
